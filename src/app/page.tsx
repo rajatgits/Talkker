b7b4236801592ffc7178c2f147683726528d47d2
Rajat Raj <rajatrajemails@gmail.com>
2024-09-16 02:27:40 +0530

    Authentication and Workspace api added

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { UserButton } from "@/features/auth/components/user-button";
+import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspace";
+import { useEffect, useMemo } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+  const [open, setOpen] = useCreateWorkspaceModal();
+  const { data, isLoading } = useGetWorkspaces();
+
+  const workSpaceId = useMemo(() => data?.[0]?._id, [data]);
+
+  useEffect(() => {
+    if (isLoading) {
+      return;
+    }
+
+    if (workSpaceId) {
+      router.replace(`/workspace/${workSpaceId}`);
+    } else if (!open) {
+      setOpen(true);
+    }
+  }, [workSpaceId, isLoading, open, setOpen, router]);
+
+  return <UserButton />;
+}
